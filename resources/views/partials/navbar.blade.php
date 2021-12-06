792572c4367d732842e83f690764771ceac05350
rizkanfirmansyah <riezkanaprianda@gmail.com>
2021-12-07 00:25:27 +0700

    added forgot password

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -10,9 +10,6 @@
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
 
-
-
-
                         <div class="topbar-divider d-none d-sm-block"></div>
 
                         <!-- Nav Item - User Information -->
